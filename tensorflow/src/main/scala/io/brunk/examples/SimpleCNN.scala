46e204a02840d77149923893c622d1d4ef06bced
Sören Brunk <soeren@brunk.io>
2018-05-01 21:05:54 +0200

    Add some fixes for the tensorflow CNN example

--- tensorflow/src/main/scala/io/brunk/examples/SimpleCNN.scala
+++ tensorflow/src/main/scala/io/brunk/examples/SimpleCNN.scala
@@ -42,9 +42,10 @@ import better.files._
 
 import scala.util.Random.shuffle
 import org.platanios.tensorflow.api.ops.io.data.{Dataset, TensorSlicesDataset}
+import org.platanios.tensorflow.api.ops.variables.GlorotUniformInitializer
 
 /**
-  * CNN image classification examples
+  * CNN for image classification example
   *
   * @author Sören Brunk
   */
@@ -53,7 +54,8 @@ object SimpleCNN {
 
   def main(args: Array[String]): Unit = {
 
-    val batchSize    = 32
+    val seed = 42
+    val batchSize    = 512
     val numEpochs    = 10
 
     val dataDir = File(args(0))
@@ -70,6 +72,10 @@ object SimpleCNN {
       }
     }
 
+    val numClasses = imgClasses.size
+    logger.info("Number of classes {}", numClasses)
+
+
     val numSamples = shuffledSamples.size
     logger.info("Number of samples {}", numSamples)
     val numTrainSamples = (0.8 * numSamples).toInt
@@ -84,82 +90,73 @@ object SimpleCNN {
 
     val dataSet: Dataset[(Tensor, Tensor), (Output, Output), (DataType, DataType), (Shape, Shape)] =
       tf.data.TensorSlicesDataset(filenames)
-      .map { filename =>
-        val rawImage = tf.data.readFile(filename)
-        val image = tf.image.decodeJpeg(rawImage)
-        image
-        // TODO resize image
-      }.zip(tf.data.TensorSlicesDataset(labels))
-
-
-    logger.info(dataSet.outputShapes._1.toString())
+        .zip(tf.data.TensorSlicesDataset(labels))
+        .shuffle(numSamples, Some(seed))
+        .map ({ case (filename, label) =>
+          val rawImage = tf.data.readFile(filename)
+          val image = tf.image.decodeJpeg(rawImage, numChannels = 3)
+          (image, label)
+          // TODO resize image
+        }, numParallelCalls = 10)
 
     val trainData = dataSet
       .take(numTrainSamples)
       .repeat()
-      //.shuffle(10000)
       .batch(batchSize)
-      .prefetch(10)
-    val evalTrainData = dataSet.batch(1000).prefetch(10)
-    val evalTestData = dataSet.drop(numTrainSamples).batch(1000).prefetch(10)
-
-    logger.info(trainData.outputShapes._1.toString())
-
+      .prefetch(100)
+    val evalTrainData = dataSet.batch(200).prefetch(100)
+    val evalTestData = dataSet.drop(numTrainSamples).batch(200).prefetch(100)
 
     // define the neural network architecture
-    val input = tf.learn.Input(UINT8, Shape(-1, dataSet.outputShapes._1(0), dataSet.outputShapes._1(1), dataSet.outputShapes._1(2))) // type and shape of images
+    val input = tf.learn.Input(UINT8, Shape(-1, 100, 100, 3)) // type and shape of images
     val trainInput = tf.learn.Input(UINT8, Shape(-1)) // type and shape of labels
 
     val layers =
       tf.learn.Cast("Input/Cast", FLOAT32) >>
       tf.learn.Conv2D("Layer_0/Conv2D", Shape(3, 3, 3, 32), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.AddBias("Layer_0/Bias") >>
-      tf.learn.ReLU("Layer_0/ReLU", alpha = 0.1f) >>
-      tf.learn.MaxPool("Layer_0/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding)
+      tf.learn.ReLU("Layer_0/ReLU") >>
+      tf.learn.MaxPool("Layer_0/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.Conv2D("Layer_1/Conv2D", Shape(3, 3, 32, 64), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.AddBias("Layer_1/Bias") >>
-      tf.learn.ReLU("Layer_1/ReLU", alpha = 0.1f) >>
-      tf.learn.MaxPool("Layer_1/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding)
+      tf.learn.ReLU("Layer_1/ReLU") >>
+      tf.learn.MaxPool("Layer_1/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.Conv2D("Layer_2/Conv2D", Shape(3, 3, 64, 128), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.AddBias("Layer_2/Bias") >>
       tf.learn.ReLU("Layer_2/ReLU", alpha = 0.1f) >>
-      tf.learn.MaxPool("Layer_2/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding)
+      tf.learn.MaxPool("Layer_2/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.Conv2D("Layer_3/Conv2D", Shape(3, 3, 128, 128), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
       tf.learn.AddBias("Layer_3/Bias") >>
       tf.learn.ReLU("Layer_3/ReLU", alpha = 0.1f) >>
-      tf.learn.MaxPool("Layer_3/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding)
-      tf.learn.Dropout("Layer_3/Dropout", keepProbability = 0.5f)
-      tf.learn.Flatten("Layer_3/Flatten")
+      tf.learn.MaxPool("Layer_3/MaxPool", windowSize = Seq(1, 2, 2, 1), stride1 = 1, stride2 = 1, padding = ValidConvPadding) >>
+      tf.learn.Dropout("Layer_3/Dropout", keepProbability = 0.5f) >>
+      tf.learn.Flatten("Layer_3/Flatten") >>
       tf.learn.Linear("Layer_4/Linear", units = 512) >> tf.learn.ReLU("Layer_4/ReLU", 0.1f) >>
-      tf.learn.Linear("OutputLayer/Linear", 1)
+      tf.learn.Linear("OutputLayer/Linear", 2)
 
-    val trainInputLayer = tf.learn.Cast("TrainInput/Cast", INT64) // cast labels to long
+    val trainInputLayer = tf.learn.Cast("TrainInput/Cast", INT32) // cast labels to long
 
-    val loss = tf.learn.SigmoidCrossEntropy("Loss/CrossEntropy") >> tf.learn.Mean("Loss/Mean") >>
-      tf.learn.ScalarSummary("Loss/Summary", "Loss")
-    val optimizer = tf.train.AdaGrad(0.1f)
+    val loss = tf.learn.SparseSoftmaxCrossEntropy("Loss/CrossEntropy") >>
+      tf.learn.Mean("Loss/Mean") >> tf.learn.ScalarSummary("Loss/Summary", "Loss")
+    val optimizer = tf.train.AdaGrad(0.001f)
 
     val model = tf.learn.Model.supervised(input, layers, trainInput, trainInputLayer, loss, optimizer)
 
-//    val loss = tf.learn.SparseSoftmaxCrossEntropy("Loss/CrossEntropy") >>
-//        tf.learn.Mean("Loss/Mean") >> tf.learn.ScalarSummary("Loss/Summary", "Loss"
-//    val model = tf.learn.Model(input, layer, trainInput, trainingInputLayer, loss, optimizer)
-
     val summariesDir = Paths.get("temp/simple-cnn")
-    //val accMetric = tf.metrics.MapMetric(
-    //  (v: (Output, Output)) => (v._1.argmax(-1), v._2), tf.metrics.Accuracy())
+    val accMetric = tf.metrics.MapMetric(
+      (v: (Output, Output)) => (v._1.argmax(-1), v._2), tf.metrics.Accuracy())
     val estimator = tf.learn.InMemoryEstimator(
       model,
       tf.learn.Configuration(Some(summariesDir)),
-      tf.learn.StopCriteria(maxSteps = Some((60000/batchSize)*numEpochs)), // due to a bug, we can't use epochs directly
+      tf.learn.StopCriteria(maxEpochs = Some(numEpochs)),
       Set(
         tf.learn.LossLogger(trigger = tf.learn.StepHookTrigger(100)),
-        //tf.learn.Evaluator(
-        //  log = true, datasets = Seq(("Train", () => evalTrainData), ("Test", () => evalTestData)),
-        //  metrics = Seq(accMetric), trigger = tf.learn.StepHookTrigger(1000), name = "Evaluator"),
+        tf.learn.Evaluator(
+          log = true, datasets = Seq(("Train", () => evalTrainData), ("Test", () => evalTestData)),
+          metrics = Seq(accMetric), trigger = tf.learn.StepHookTrigger(100), name = "Evaluator"),
         tf.learn.StepRateLogger(log = false, summaryDir = summariesDir, trigger = tf.learn.StepHookTrigger(100)),
         tf.learn.SummarySaver(summariesDir, tf.learn.StepHookTrigger(100)),
-        tf.learn.CheckpointSaver(summariesDir, tf.learn.StepHookTrigger(1000))),
+        tf.learn.CheckpointSaver(summariesDir, tf.learn.StepHookTrigger(100))),
       tensorBoardConfig = tf.learn.TensorBoardConfig(summariesDir, reloadInterval = 1))
 
     // train the model
